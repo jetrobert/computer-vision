fbcde17c13d9223735bcf3b4b03af15645007912
Stephen Cheng <jetrobert19@gmail.com>
2017-06-11 18:26:59 +0800

    Add files via upload

---
+++ ict/19_Deep_Neural_Network_Framwork_Benchmark/tf/inception-v3-imagenet/slim.py
@@ -0,0 +1,10 @@
+"""TF-Slim grouped API. Please see README.md for details and usage."""
+# pylint: disable=unused-import
+
+# Collapse tf-slim into a single namespace.
+import inception_model as inception
+import losses
+import ops
+import scopes
+import variables
+from scopes import arg_scope
