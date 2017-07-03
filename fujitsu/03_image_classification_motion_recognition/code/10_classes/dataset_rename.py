d1c9811b80096ee4a2a2b78fe4aaf50ffdf3214f
Stephen Cheng <jetrobert19@gmail.com>
2017-07-03 19:03:48 +0800

    Add files via upload

---
+++ fujitsu/03_image_classification_motion_recognition/code/10_classes/dataset_rename.py
@@ -0,0 +1,21 @@
+import os
+
+def file_rename(path, label_num):
+	filenames = os.listdir(path)
+	for name in filenames:
+		print(name)
+	for num in range(0,len(filenames)):
+		if(num<10):
+			print()
+			print(filenames[num])
+			os.rename(path+filenames[num], path+str(label_num)+'_'+'0'+str(num)+'.jpg')
+		else:
+			os.rename(path+filenames[num], path+str(label_num)+'_'+str(num)+'.jpg')
+			
+if __name__ == '__main__':
+	label_names = ['D02WalkingQuickly', 'D04JogingQuickly', 'D06DownstairsQuickly', 
+				   'D06UpstairsQuickly', 'D14LyBack', 'D14LyLateral', 'D15Standing', 
+				   'D18Stumble', 'D19Jump', 'D1213Sit']
+	for label_num in range(len(label_names)):
+		path = '../../dataset/10_classes/dataset_process/'+label_names[label_num]+'/'
+		file_rename(path, label_num)
