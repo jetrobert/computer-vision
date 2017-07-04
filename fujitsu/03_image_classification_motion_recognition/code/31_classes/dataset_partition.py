c118a2426d5ddc99aa16ebd06f14488dcdec651d
Stephen Cheng <jetrobert19@gmail.com>
2017-07-04 20:35:05 +0800

    Add files via upload

---
+++ fujitsu/03_image_classification_motion_recognition/code/31_classes/dataset_partition.py
@@ -0,0 +1,37 @@
+import os
+import random
+import shutil
+
+def file_move(path, path_train, path_test, label_num):
+	filenames = os.listdir(path)
+	test_num = len(filenames)//6
+	random_num = random.sample(range(10,len(filenames)),test_num)
+	
+	# randomly choose test data
+	for num_ in random_num:
+		file = path+str(label_num)+'_'+str(num_)+'.jpg'
+		shutil.move(file, path_test)
+		print "move %s to %s successful" % (file, path_test)
+		
+	# the rest is train data
+	for name in filenames:
+		#print(name)
+		if os.path.isfile(path+name):     
+			#shutil.copy(path+name, path_target)
+			shutil.move(path+name, path_train)
+			print "move %s to %s successful" % (name, path_train)
+			
+if __name__ == '__main__':
+	label_names = ['D01WalkingSlowly', 'D02WalkingQuickly', 'D03JogingSlowly', 'D04JogingQuickly', \
+				'D05DownstairsSlowly', 'D05UpstairsSlowly', 'D06DownstairsQuickly', 'D06UpstairsQuickly', \
+				'D08HalfSitDownQuickly', 'D08HalfSitUpQuickly', 'D10LowSitDownQuickly', 'D10LowSitUpQuickly', \
+				'D12LyLefttoSitSlowly', 'D12SitToLyLeftSlowly', 'D13LyLefttoSitQuickly', 'D13SittoLyLeftQuickly', \
+				'D14LyBack', 'D14LyBacktoLateral', 'D14LyLateral', 'D14LyLateraltoBack', \
+				'D15Bending', 'D15BendingtoStanding', 'D15Standing', 'D15StandtoBending', \
+				'D16BendDown', 'D16BendUp', 'D18Stumble', 'D18Walking', \
+				'D19Jump', 'D1213LyLeft', 'D1213Sit']
+	for label_num in range(len(label_names)):
+		path = '../dataset/dataset_p/'+label_names[label_num]+'/'
+		path_train = '../dataset/dataset_train'
+		path_test = '../dataset/dataset_test'
+		file_move(path, path_train, path_test, label_num)
---
+++ fujitsu/03_image_classification_motion_recognition/code/31_classes/meta_info.py
@@ -0,0 +1,21 @@
+import cPickle 
+
+def main(data_size, path):   
+    label_names = ['WalkingSlowly', 'WalkingQuickly', 'JogingSlowly', 'JogingQuickly', \
+				'DownstairsSlowly', 'UpstairsSlowly', 'DownstairsQuickly', 'UpstairsQuickly', \
+				'HalfSitDownQuickly', 'HalfSitUpQuickly', 'LowSitDownQuickly', 'LowSitUpQuickly', \
+				'LyLefttoSitSlowly', 'SitToLyLeftSlowly', 'LyLefttoSitQuickly', 'SittoLyLeftQuickly', \
+				'LyBack', 'LyBacktoLateral', 'LyLateral', 'LyLateraltoBack', 'Bending', \
+				'BendingtoStanding', 'Standing', 'StandtoBending', 'BendDown', 'BendUp', \
+				'Stumble', 'Walking', 'Jump', 'LyLeft', 'Sit']  
+    num1 = 1800  
+    num2 = data_size * data_size * 3  
+    dic = {'num_cases_per_batch': num1, 'label_names':label_names, 'num_vis':num2}  
+    out_file = open(path+'/batches.meta', 'w+')  
+    cPickle.dump(dic, out_file)  
+    out_file.close()  
+	
+if __name__ == '__main__':
+    data_size = 256
+    path = '../dataset/dataset-batches-py-256'
+    main(data_size, path)
